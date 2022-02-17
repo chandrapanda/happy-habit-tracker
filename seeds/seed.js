3f2898790f342c8b6a154e612160e152d4dd9119
Nick Edwards <theadventuresofnickedwards@gmail.com>
2022-02-16 21:41:05 -0800

    Updates seeds.js to match correct order of creation, adjusted timestamp formats in seeds, adjusted tiemstamp datatype

--- seeds/seed.js
+++ seeds/seed.js
@@ -1,27 +1,27 @@
-const sequelize = require ("../config/connection");
-const {Category, Entry, Habit } = require ("../models");
+const sequelize = require ('../config/connection');
+const {Category, Entry, Habit, User} = require ('../models');
 
-const categoryData = require ("./categoryData.json");
+const categoryData = require ('./categoryData.json');
 const habitData = require('./habitData.json');
 const entryData = require('./entryData.json');
 const userData = require('./userData.json');
 
 const seeds = async () => {
     await sequelize.sync ({ force:true});
+    
+    await Category.bulkCreate(categoryData, {returning:true})
+    console.log('Categroies Seeded');
 
-   await Category.bulkCreate(categoryData, {returning:true})
-   console.log('Categroies Seeded');
+    await Habit.bulkCreate(habitData, {returning: true})
+    console.log('Habits Seeded');
 
-   await Habit.bulkCreate(habitData, {returning: true})
-   console.log('Habits Seeded');
+    await User.bulkCreate(userData, {returning: true});
+    console.log('user Seeded');
 
-   await Entry.bulkCreate(entryData, {returning: true});
-   console.log('Entries Seeded');
-
-   await Entry.bulkCreate(userData, {returning: true});
-   console.log('user Seeded');
+    await Entry.bulkCreate(entryData, {returning: true});
+    console.log('Entries Seeded');
    
-   process.exit();
+    process.exit();
 }
 
 seeds();
