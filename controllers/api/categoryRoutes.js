ec7b02c2b7bb20ed32ee197507bace5b063b4a71
Chandra Holt <chandra_holt@hotmail.com>
2022-02-16 18:53:41 -0800

    Preliminary Entry routes and Category Routes created

--- controllers/api/categoryRoutes.js
+++ controllers/api/categoryRoutes.js
@@ -1,1 +1,51 @@
 // TODO: CATEGORY ROUTES
+
+const router = require("express").Router();
+const { Category, Product } = require("../../models");
+
+// The `/api/categories` endpoint
+
+router.get("/", async (req, res) => {
+  //Find all categories
+  try {
+    const categoryData = await Category.findAll({
+      fields: ["id"],
+      // be sure to include its associated Products
+      include: [{ model: Product }],
+    });
+    res.status(200).json(categoryData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+//Get one category
+router.get("/:id", async (req, res) => {
+  // Find one category by its `id` value
+  try {
+    const categoryData = await Category.findByPk(req.params.id, {
+      // be sure to include its associated Products
+      include: [{ model: Product }],
+    });
+    if (!categoryData) {
+      res.status(404).json({ message: "No category with this id." });
+      return;
+    }
+    res.status(200).json(categoryData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+//Create a new category
+router.post("/", async (req, res) => {
+  try {
+    const newCategory = await Category.create(req.body);
+    res.status(200).json(newCategory);
+  } catch (err) {
+    console.log(err);
+    res.status(500).json(err);
+  }
+});
+
+module.exports = router;
--- controllers/api/entryRoutes.js
+++ controllers/api/entryRoutes.js
@@ -1,1 +1,80 @@
 // TODO: ENTRY ROUTES
+
+const router = require('express').Router();
+const { Entry, Habit } = require('../../models');
+
+// The `/api/entries` endpoint
+
+router.get('/', async (req, res) => {
+    //Find all categories
+  try {
+    const entryData = await Entry.findAll({
+      fields: ['id'],
+      // be sure to include its associated Habit
+      include: [{ model: Habit }]
+    });
+    res.status(200).json(entryData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+//Get one category
+router.get('/:id', async (req, res) => {
+  // Find one category by its `id` value
+  try {
+    const entryData = await Entry.findByPk(req.params.id, {
+      // be sure to include its associated Products
+      include: [{ model: Habit }]
+    });
+    if (!entryData) {
+      res.status(404).json({message: "No category with this id."});
+      return;
+    }
+    res.status(200).json(entryData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+//Create a new category
+router.post('/', async (req, res) => {
+  try {
+    const newEntry = await Entry.create(req.body);
+    res.status(200).json(newEntry);
+  } catch (err) {
+    console.log(err);
+    res.status(500).json(err);
+  }
+});
+
+//Update category by its `id` value
+router.put('/:id', async (req, res) => {
+  try {
+    const updatedCategory = await Category.update(req.body, {
+      where: {id: req.params.id,}
+    });
+    res.status(200).json(updatedCategory);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+router.delete('/:id', async (req, res) => {
+  // delete a category by its `id` value
+  try {
+    const entryData = await Entry.destroy({
+      where: { id: req.params.id }
+    });
+    if (!entryData) {
+      res.status(404).json({message: 'Entry with this ID not found.' 
+    });
+      return;
+    }
+    res.status(200).json(entryData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+module.exports = router;
