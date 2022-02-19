dce96f316dc251b2d7c5d0e5c84a394c1b6bb03c
Sawyer Silfies <ssilfies17@gmail.com>
2022-02-19 11:12:55 -0800

    Homepage matches rest of application theme and structure. Reorganizing front end files. Multipe copies of utility files.

--- html-pages/script.js
+++ html-pages/script.js
@@ -12,5 +12,19 @@ const currentDateEl = document.getElementById('current-date');
 currentDateEl.innerHTML = dateString;
 
 
+const physicalCard = document.querySelector("card1");
+const mentalCard = document.querySelector("card2");
+const dietaryCard = document.querySelector("card3"); 
 
+physicalCard.addEventListener("click", function () {
+
+});
+
+mentalCard.addEventListener("click", function () {
+
+});
+
+dietaryCard.addEventListener("click", function () {
+
+});
 
