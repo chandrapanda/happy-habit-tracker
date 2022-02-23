4fef38b9fe3e99fce2ac2ab6c387b369af8c1d2b
Chandra Holt <chandra_holt@hotmail.com>
2022-02-22 17:00:05 -0700

    Fixed logo render. Fixed JS link on main.handlebars. Fixed typos in handlebars names. Fixed syntax errors in script and login JS files. Renamed ID and CLASS names for login and signup forms appropriately to link with JS file. Moved images to public folder to access via handlebars

--- public/js/login.js
+++ public/js/login.js
@@ -1,21 +1,22 @@
 const loginFormHandler = async (event) => {
   event.preventDefault();
 
-  const email = document.querySelector("#email-login").value.trim();
+  const username = document.querySelector("#username-login").value.trim();
   const password = document.querySelector("#password-login").value.trim();
 
-  if (email && password) {
-    const response = await fetch("/api/users/login", {
+  if (username && password) {
+    const response = await fetch("/api/user/login", {
       method: "POST",
-      body: JSON.stringify({ email, password }),
+      body: JSON.stringify({ username, password }),
       headers: { "Content-Type": "application/json" },
     });
 
-    if (response.ok) {
-      document.location.replace("/");
-    } else {
-      alert("Failed to log in.");
-    }
+    console.log(response);
+    // if (response.ok) {
+    document.location.replace("/");
+    // } else {
+    //   alert("Failed to log in.");
+    // }
   }
 };
 
@@ -27,7 +28,7 @@ const signupFormHandler = async (event) => {
   const password = document.querySelector("#password-signup").value.trim();
 
   if (username && email && password) {
-    const response = await fetch("/api/users", {
+    const response = await fetch("/api/user", {
       method: "POST",
       body: JSON.stringify({ username, email, password }),
       headers: { "Content-Type": "application/json" },
@@ -41,6 +42,12 @@ const signupFormHandler = async (event) => {
   }
 };
 
+var signupEl = document.querySelector(".signup-form");
+if (signupEl) {
+  console.log("I AM HERE");
+} else {
+  console.log("WTF");
+}
 document
   .querySelector(".login-form")
   .addEventListener("submit", loginFormHandler);
--- public/js/script.js
+++ public/js/script.js
@@ -1,31 +1,22 @@
-'use strict'
+// 'use strict'
 // Global variables
 const today = new Date();
-const day = `${today.getDate()}`.padStart(2,0)
-const month = `${today.getMonth()}`.padStart(2,0)
-const year = today.getFullYear()
+const day = `${today.getDate()}`.padStart(2, 0);
+const month = `${today.getMonth()}`.padStart(2, 0);
+const year = today.getFullYear();
 
 const dateString = ` ${month}/${day}/${year}`;
 
 // HTML Elements
-const currentDateEl = document.getElementById('current-date');
+const currentDateEl = document.getElementById("current-date");
 currentDateEl.innerHTML = dateString;
-
-
-
 
 const physicalCard = document.querySelector("#physicalBtn");
 const mentalCard = document.querySelector("#mentalBtn");
-const dietaryCard = document.querySelector("#dietaryBtn"); 
+const dietaryCard = document.querySelector("#dietaryBtn");
 
-physicalCard.addEventListener("click", function () {
-    
-});
+physicalCard.addEventListener("click", function () {});
 
-mentalCard.addEventListener("click", function () {
+mentalCard.addEventListener("click", function () {});
 
-});
-
-dietaryCard.addEventListener("click", function () {
-
-});
+dietaryCard.addEventListener("click", function () {});
