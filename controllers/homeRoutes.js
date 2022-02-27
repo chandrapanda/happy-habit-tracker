b9773f2f4ed5dc12bc88c564267c809cd16f1d9e
Nick Edwards <theadventuresofnickedwards@gmail.com>
2022-02-27 12:27:45 -0800

    Render for entries across all habits complete

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -103,7 +103,7 @@ router.get("/physical/running", withAuth, async (req, res) => {
   }
 });
 // Yoga route all entries by user
-router.get("/physical/yoga", async (req, res) => {
+router.get("/physical/yoga", withAuth, async (req, res) => {
   try {
     const entryDataByUser = await Entry.findAll({
       include: [
@@ -134,87 +134,316 @@ router.get("/physical/yoga", async (req, res) => {
   }
 });
 // Weightlifting route all entries by user
-router.get("/physical/weightLifting", (req, res) => {
-  res.render("habitPage", {
-    title: "Weight Lifting",
-    categoryID: 1,
-    habitID: 3,
-  });
+router.get("/physical/weightLifting", withAuth, async (req, res) => {
+  try {
+    const entryDataByUser = await Entry.findAll({
+      include: [
+        { model: User, attributes: ['id'] },
+        { model: Habit, attributes: ['habit_name']}
+      ],
+      where: {
+        user_id: req.session.user_id,
+        habit_id: 2
+      },
+    });
+  
+    const entries = entryDataByUser.map((entry) => entry.get({ plain: true }));
+
+    res.render("habitPage", {
+      entries,
+      title: "Weight Lifting",
+      categoryID: 1,
+      habitID: 3,
+      logged_in: req.session.logged_in
+    }
+    );
+    // res.status(200).json(entries);
+  } catch (err) {
+    console.log(err)
+    // res.status(500).json(err);
+    res.redirect('login');
+  }
 });
 // Other physical route all entries by user
-router.get("/physical/other", (req, res) => {
-  res.render("habitPage", {
-    title: "Other",
-    categoryID: 1,
-    habitID: 4,
-  });
-});
+router.get("/physical/other", withAuth, async (req, res) => {
+  try {
+    const entryDataByUser = await Entry.findAll({
+      include: [
+        { model: User, attributes: ['id'] },
+        { model: Habit, attributes: ['habit_name']}
+      ],
+      where: {
+        user_id: req.session.user_id,
+        habit_id: 2
+      },
+    });
+  
+    const entries = entryDataByUser.map((entry) => entry.get({ plain: true }));
 
+    res.render("habitPage", {
+      entries,
+      title: "Other - Physical",
+      categoryID: 1,
+      habitID: 4,
+      logged_in: req.session.logged_in
+    }
+    );
+    // res.status(200).json(entries);
+  } catch (err) {
+    console.log(err)
+    // res.status(500).json(err);
+    res.redirect('login');
+  }
+});
 // MENTAL ROUTES START HERE
 // Meditation route all entries by user
-router.get("/mental/meditation", (req, res) => {
-  res.render("habitPage", {
-    title: "Meditation",
-    categoryID: 2,
-    habitID: 5,
-  });
+router.get("/mental/meditation", withAuth, async (req, res) => {
+  try {
+    const entryDataByUser = await Entry.findAll({
+      include: [
+        { model: User, attributes: ['id'] },
+        { model: Habit, attributes: ['habit_name']}
+      ],
+      where: {
+        user_id: req.session.user_id,
+        habit_id: 2
+      },
+    });
+  
+    const entries = entryDataByUser.map((entry) => entry.get({ plain: true }));
+
+    res.render("habitPage", {
+      entries,
+      title: "Meditation",
+      categoryID: 2,
+      habitID: 5,
+      logged_in: req.session.logged_in
+    }
+    );
+    // res.status(200).json(entries);
+  } catch (err) {
+    console.log(err)
+    // res.status(500).json(err);
+    res.redirect('login');
+  }
 });
 // Journaling route all entries by user
-router.get("/mental/journaling", (req, res) => {
-  res.render("habitPage", {
-    title: "Journaling ",
-    categoryID: 2,
-    habitID: 6,
-  });
+router.get("/mental/journaling", withAuth, async (req, res) => {
+  try {
+    const entryDataByUser = await Entry.findAll({
+      include: [
+        { model: User, attributes: ['id'] },
+        { model: Habit, attributes: ['habit_name']}
+      ],
+      where: {
+        user_id: req.session.user_id,
+        habit_id: 2
+      },
+    });
+  
+    const entries = entryDataByUser.map((entry) => entry.get({ plain: true }));
+
+    res.render("habitPage", {
+      entries,
+      title: "Journaling",
+      categoryID: 2,
+      habitID: 6,
+      logged_in: req.session.logged_in
+    }
+    );
+    // res.status(200).json(entries);
+  } catch (err) {
+    console.log(err)
+    // res.status(500).json(err);
+    res.redirect('login');
+  }
 });
 // Rest route all entries by user
-router.get("/mental/rest", (req, res) => {
-  res.render("habitPage", {
-    title: "Rest ",
-    categoryID: 2,
-    habitID: 7,
-  });
+router.get("/mental/rest", withAuth, async (req, res) => {
+  try {
+    const entryDataByUser = await Entry.findAll({
+      include: [
+        { model: User, attributes: ['id'] },
+        { model: Habit, attributes: ['habit_name']}
+      ],
+      where: {
+        user_id: req.session.user_id,
+        habit_id: 2
+      },
+    });
+  
+    const entries = entryDataByUser.map((entry) => entry.get({ plain: true }));
+
+    res.render("habitPage", {
+      entries,
+      title: "Rest",
+      categoryID: 2,
+      habitID: 7,
+      logged_in: req.session.logged_in
+    }
+    );
+    // res.status(200).json(entries);
+  } catch (err) {
+    console.log(err)
+    // res.status(500).json(err);
+    res.redirect('login');
+  }
 });
 // Other mental route all entries by user
-router.get("/mental/other", (req, res) => {
-  res.render("habitPage", {
-    title: "Other ",
-    categoryID: 2,
-    habitID: 8,
-  });
-});
+router.get("/mental/other", withAuth, async (req, res) => {
+  try {
+    const entryDataByUser = await Entry.findAll({
+      include: [
+        { model: User, attributes: ['id'] },
+        { model: Habit, attributes: ['habit_name']}
+      ],
+      where: {
+        user_id: req.session.user_id,
+        habit_id: 2
+      },
+    });
+  
+    const entries = entryDataByUser.map((entry) => entry.get({ plain: true }));
 
+    res.render("habitPage", {
+      entries,
+      title: "Other - Menal",
+      categoryID: 2,
+      habitID: 8,
+      logged_in: req.session.logged_in
+    }
+    );
+    // res.status(200).json(entries);
+  } catch (err) {
+    console.log(err)
+    // res.status(500).json(err);
+    res.redirect('login');
+  }
+});
 // DIETARY ROUTES START HERE
 // Water route all entries by user
-router.get("/dietary/water", (req, res) => {
-  res.render("habitPage", {
-    title: "Water ",
-    categoryID: 3,
-    habitID: 9,
-  });
+router.get("/dietary/water", withAuth, async (req, res) => {
+  try {
+    const entryDataByUser = await Entry.findAll({
+      include: [
+        { model: User, attributes: ['id'] },
+        { model: Habit, attributes: ['habit_name']}
+      ],
+      where: {
+        user_id: req.session.user_id,
+        habit_id: 2
+      },
+    });
+  
+    const entries = entryDataByUser.map((entry) => entry.get({ plain: true }));
+
+    res.render("habitPage", {
+      entries,
+      title: "Water",
+      categoryID: 3,
+      habitID: 9,
+      logged_in: req.session.logged_in
+    }
+    );
+    // res.status(200).json(entries);
+  } catch (err) {
+    console.log(err)
+    // res.status(500).json(err);
+    res.redirect('login');
+  }
 });
 // Fruit & Veg route all entries by user
-router.get("/dietary/fruit&veggie", (req, res) => {
-  res.render("habitPage", {
-    title: "Fruit & Veggie ",
-    categoryID: 3,
-    habitID: 10,
-  });
+router.get("/dietary/fruit&veggie", withAuth, async (req, res) => {
+  try {
+    const entryDataByUser = await Entry.findAll({
+      include: [
+        { model: User, attributes: ['id'] },
+        { model: Habit, attributes: ['habit_name']}
+      ],
+      where: {
+        user_id: req.session.user_id,
+        habit_id: 2
+      },
+    });
+  
+    const entries = entryDataByUser.map((entry) => entry.get({ plain: true }));
+
+    res.render("habitPage", {
+      entries,
+      title: "Fruits & Veggies",
+      categoryID: 3,
+      habitID: 10,
+      logged_in: req.session.logged_in
+    }
+    );
+    // res.status(200).json(entries);
+  } catch (err) {
+    console.log(err)
+    // res.status(500).json(err);
+    res.redirect('login');
+  }
 });
 // Protein route all entries by user
-router.get("/dietary/protiein", (req, res) => {
-  res.render("habitPage", {
-    title: "Protein ",
-    categoryID: 3,
-    habitID: 11,
-  });
+router.get("/dietary/protiein", withAuth, async (req, res) => {
+  try {
+    const entryDataByUser = await Entry.findAll({
+      include: [
+        { model: User, attributes: ['id'] },
+        { model: Habit, attributes: ['habit_name']}
+      ],
+      where: {
+        user_id: req.session.user_id,
+        habit_id: 2
+      },
+    });
+  
+    const entries = entryDataByUser.map((entry) => entry.get({ plain: true }));
+
+    res.render("habitPage", {
+      entries,
+      title: "Protein",
+      categoryID: 3,
+      habitID: 11,
+      logged_in: req.session.logged_in
+    }
+    );
+    // res.status(200).json(entries);
+  } catch (err) {
+    console.log(err)
+    // res.status(500).json(err);
+    res.redirect('login');
+  }
 });
 // Other dietary route all entries by user
-router.get("/dietary/other", (req, res) => {
-  res.render("habitPage", {
-    title: "Other ",
-    categoryID: 3,
-    habitID: 12
-  });
+router.get("/dietary/other", withAuth, async (req, res) => {
+  try {
+    const entryDataByUser = await Entry.findAll({
+      include: [
+        { model: User, attributes: ['id'] },
+        { model: Habit, attributes: ['habit_name']}
+      ],
+      where: {
+        user_id: req.session.user_id,
+        habit_id: 2
+      },
+    });
+  
+    const entries = entryDataByUser.map((entry) => entry.get({ plain: true }));
+
+    res.render("habitPage", {
+      entries,
+      title: "Other - Dietary",
+      categoryID: 3,
+      habitID: 12,
+      logged_in: req.session.logged_in
+    }
+    );
+    // res.status(200).json(entries);
+  } catch (err) {
+    console.log(err)
+    // res.status(500).json(err);
+    res.redirect('login');
+  }
 });
+
 module.exports = router;
