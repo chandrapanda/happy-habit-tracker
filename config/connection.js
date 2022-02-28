0656cbd39a3fbc0231fb1aa8ea610a6a3f7a9964
Chandra Holt <chandra_holt@hotmail.com>
2022-02-28 15:40:29 -0800

    Commented out local server code

--- config/connection.js
+++ config/connection.js
@@ -16,16 +16,16 @@ const sequelize = process.env.JAWSDB_URL
       }
     );
 
-const sequelizeLocal = new Sequelize(
-  process.env.DB_NAME,
-  process.env.DB_USER,
-  process.env.DB_PASSWORD,
-  {
-    host: "127.0.0.1",
-    dialect: "mysql",
-    port: 3306,
-  }
-);
+// const sequelizeLocal = new Sequelize(
+//   process.env.DB_NAME,
+//   process.env.DB_USER,
+//   process.env.DB_PASSWORD,
+//   {
+//     host: "127.0.0.1",
+//     dialect: "mysql",
+//     port: 3306,
+//   }
+// );
 
-module.exports = sequelizeLocal;
+// module.exports = sequelizeLocal;
 module.exports = sequelize;
