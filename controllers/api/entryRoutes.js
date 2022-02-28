3eff5fa27442a67880ec0d19bbaae966583dff88
deluus polycarp <pdeluus@gmail.com>
2022-02-27 20:23:33 -0800

    cleaned up used codes. addhabit.html, addhabit, times js,

--- controllers/api/entryRoutes.js
+++ controllers/api/entryRoutes.js
@@ -36,12 +36,7 @@ router.get("/dashboard/", async (req, res) => {
     });
   
     const entries = entryDataByUser.map((entry) => entry.get({ plain: true }));
-    // res.render('dashboard', {
-      //   // TODO: Which layout in handlebars is used to show the user all their entries???
-      //   layout: 'main', 
-      //   entries,
-      // });
-      
+    
     res.status(200).json(entryDataByUser);
   } catch (err) {
     res.status(500).json(err);
@@ -65,12 +60,7 @@ router.get("/tracking/:id", async (req, res) => {
       },
     });
   
-    const entries = entryDataByUser.map((entry) => entry.get({ plain: true }));
-    // res.render('habitPage', {
-      //   // TODO: Which layout in handlebars is used to show the user all their entries???
-      //   layout: 'main', 
-      //   entries,
-      // });
+    const entries = entryDataByUser.map((entry) => entry.get({ plain: true })); 
       
     res.status(200).json(entryDataByUser);
   } catch (err) {
--- public/js/addHabit.js
+++ public/js/addHabit.js
@@ -1,32 +1,14 @@
-// const user_id = document.getElementById("user_id").value;
-// const habit_id = document.getElementById("habit_id").value;
-
-
-// document.getElementById("myBtn").addEventListener("click", displayDate);
-// const date = moment().format('YYYY MM DD, H:mm:ss');
-// console.log(date)
-// const backToDate = new Date (jsonDate);
-// console.log(backToDate)
-
-// function displayDate() {
-//   document.getElementById("add").innerHTML = Date();
-//   console.log(Date);
-// }
-
-const addEntry = async function(event) {
-  const habit_id = event.currentTarget.dataset.id
+const addEntry = async function (event) {
+  const habit_id = event.currentTarget.dataset.id;
   console.log(habit_id);
 
   await fetch(`/api/entry`, {
-      method: 'POST',
-      body: JSON.stringify({habit_id}),
-      headers: {
-        'Content-Type': 'application/json',
-      },
-    });
-}
+    method: "POST",
+    body: JSON.stringify({ habit_id }),
+    headers: {
+      "Content-Type": "application/json",
+    },
+  });
+};
 
-  document
-  .querySelector('#myBtn')
-  .addEventListener('click', addEntry
-  );
+document.querySelector("#myBtn").addEventListener("click", addEntry);
